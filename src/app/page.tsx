d340933acc388be5fb1b6295abc4656dd51fb62a
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2024-12-24 19:21:51 +0100

    hydratation suppressor en home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,20 @@ import VideoList from "@/components/video/video-list";
 
 type SearchParams = Promise<{ page: string }>;
 
+const HydrationSuppressor = ({ children }: { children: React.ReactNode }) => {
+  return <div suppressHydrationWarning>{children}</div>;
+};
+
 const HomePage = async ({ searchParams }: { searchParams: SearchParams }) => {
   const { page } = await searchParams;
   const pageNumber = parseInt(page ?? "1", 10);
   const { videos, totalPages } = await serverClient.video.videoList.query({page: pageNumber});
 
-  return <VideoList videos={videos} totalPages={totalPages} page={pageNumber} />
+  return (
+    <HydrationSuppressor>
+      <VideoList videos={videos} totalPages={totalPages} page={pageNumber} />
+    </HydrationSuppressor>
+  );
 };
 
 export default HomePage;
