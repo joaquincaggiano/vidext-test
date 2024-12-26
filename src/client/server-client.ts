6be366b937213df45fd13816a75197bed7ad6cb7
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2024-12-26 18:50:33 +0100

    env + server client modify

--- src/client/server-client.ts
+++ src/client/server-client.ts
@@ -4,7 +4,7 @@ import { type AppRouter } from "../server";
 export const serverClient = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: "ep-super-grass-a45t03v0-pooler.us-east-1.aws.neon.tech/api/trpc",
+      url: `${process.env.DATABASE_URL}/api/trpc`,
     }),
   ],
 });
