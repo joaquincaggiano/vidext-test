90b187e42c5d21c82a7bb7f10309a1d5e34782d6
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2024-12-26 18:38:48 +0100

    trcp client endpoint

--- src/client/server-client.ts
+++ src/client/server-client.ts
@@ -4,7 +4,7 @@ import { type AppRouter } from "../server";
 export const serverClient = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: "http://localhost:3000/api/trpc",
+      url: "ep-super-grass-a45t03v0-pooler.us-east-1.aws.neon.tech/api/trpc",
     }),
   ],
 });
