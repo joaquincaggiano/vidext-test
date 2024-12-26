5b0917f3c2f4ae2c7bb76e3ea1d9d30b35637a83
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2024-12-26 22:05:31 +0100

    url del provider

--- src/components/providers/Provider.tsx
+++ src/components/providers/Provider.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useState } from "react";
 import { trpc } from "@/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -10,7 +11,7 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: `${process.env.NEXT_PUBLIC_APP_URL}/api/trpc`,
         }),
       ],
     })
